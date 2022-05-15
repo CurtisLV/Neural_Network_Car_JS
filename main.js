bff01e7eed87e656e34620af2dc31b4a5696d64e
Karlis <curtislv@gmail.com>
2022-05-15 20:19:02 +0300

    adding utils file

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
 
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+
 const car = new Car(100, 100, 30, 50);
 car.draw(ctx);
 
@@ -13,6 +15,7 @@ function animate() {
     car.update();
 
     canvas.height = window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 }

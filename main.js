f8d936ed6c42afc0b37e2534afd5315ac47b1b3e
Karlis <curtislv@gmail.com>
2022-05-15 19:57:11 +0300

    15.05.2022. 19.57
    Added all the trigonometrics for car to feel drivable

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector('#myCanvas');
-canvas.height = window.innerHeight;
+
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
@@ -11,5 +11,8 @@ animate();
 
 function animate() {
     car.update();
+
+    canvas.height = window.innerHeight;
     car.draw(ctx);
+    requestAnimationFrame(animate);
 }

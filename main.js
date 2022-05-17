87204c557ad3a713498046057b394815f7aec7cc
Karlis <curtislv@gmail.com>
2022-05-17 23:15:21 +0300

    17.05.2022. 23.15
    Added border checking rays

--- main.js
+++ main.js
@@ -3,11 +3,8 @@ const canvas = document.querySelector('#myCanvas');
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
-
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-
 const car = new Car(road.getLaneCenter(1), 100, 30, 50);
-car.draw(ctx);
 
 animate();
 
@@ -17,14 +14,11 @@ function animate() {
     canvas.height = window.innerHeight;
 
     ctx.save();
-
-    // moving lines
     ctx.translate(0, -car.y + canvas.height * 0.7);
 
     road.draw(ctx);
     car.draw(ctx);
 
     ctx.restore();
-
     requestAnimationFrame(animate);
 }
--- sensor.js
+++ sensor.js
@@ -2,11 +2,10 @@ class Sensor {
     constructor(car) {
         this.car = car;
         this.rayCount = 5;
-        this.rayLength = 150;
+        this.rayLength = 200;
         this.raySpread = Math.PI / 2; // 45deg
 
         this.rays = [];
-        this.update();
         this.readings = [];
     }
 

7657c7cd7779077645712859c45819d79917b576
Karlis <curtislv@gmail.com>
2022-05-22 13:43:35 +0300

    13:43 22.05.2022.
    Giving stearing wheel to Neural network

--- network.js
+++ network.js
@@ -1,7 +1,7 @@
 class NeuralNetwork {
     constructor(neuronCounts) {
         this.levels = [];
-        for (let i = 0; i < neuronCounts.length; i++) {
+        for (let i = 0; i < neuronCounts.length - 1; i++) {
             this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]));
         }
     }
@@ -9,7 +9,7 @@ class NeuralNetwork {
     static feedForward(givenInputs, network) {
         let outputs = Level.feedForward(givenInputs, network.levels[0]);
         for (let i = 1; i < network.levels.length; i++) {
-            outputs = Level.feedForward(outputs.levels[i]);
+            outputs = Level.feedForward(outputs, network.levels[i]);
         }
         return outputs;
     }

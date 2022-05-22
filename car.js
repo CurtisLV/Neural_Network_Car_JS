71c6f2ac4e71b3baa915a8a0583bf5c729101d16
Karlis <curtislv@gmail.com>
2022-05-22 13:24:08 +0300

    13:24 22.05.2022.
    Adding Neural Network class
    Linking Neural network to our car

--- car.js
+++ car.js
@@ -15,6 +15,7 @@ class Car {
 
         if (controlType !== 'DUMMY') {
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
         }
 
         this.controls = new Controls(controlType);
@@ -28,6 +29,9 @@ class Car {
         }
         if (this.sensor) {
             this.sensor.update(roadBorders, traffic);
+            const offsets = this.sensor.readings.map((s) => {
+                return s == null ? 0 : 1 - s.offset;
+            });
         }
     }
 

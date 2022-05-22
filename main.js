33ea37aa65ec54aef46cef3861b67afda81ce2b5
Karlis <curtislv@gmail.com>
2022-05-22 13:55:49 +0300

    13.55 22.05.2022.
    Creating vizualizer for neural network

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const canvas = document.querySelector('#myCanvas');
+const carCanvas = document.querySelector('#carCanvas');
+carCanvas.width = 200;
+const networkCanvas = document.querySelector('#networkCanvas');
+networkCanvas.width = 400;
 
-canvas.width = 200;
+const carCtx = carCanvas.getContext('2d');
+const networkCtx = networkCanvas.getContext('2d');
 
-const ctx = canvas.getContext('2d');
-const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, 'AI');
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2)];
 
@@ -15,18 +18,21 @@ function animate() {
     }
     car.update(road.borders, traffic);
 
-    canvas.height = window.innerHeight;
+    carCanvas.height = window.innerHeight;
+    networkCanvas.height = window.innerHeight;
 
-    ctx.save();
-    ctx.translate(0, -car.y + canvas.height * 0.7);
+    carCtx.save();
+    carCtx.translate(0, -car.y + carCanvas.height * 0.7);
 
-    road.draw(ctx);
+    road.draw(carCtx);
 
     for (let i = 0; i < traffic.length; i++) {
-        traffic[i].draw(ctx, 'red');
+        traffic[i].draw(carCtx, 'red');
     }
 
-    car.draw(ctx, 'blue');
-    ctx.restore();
+    car.draw(carCtx, 'blue');
+    carCtx.restore();
+
+    Visualizer.drawNetwork(networkCtx, car.brain);
     requestAnimationFrame(animate);
 }

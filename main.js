a8a7818827d57894c2946ed021d4e62e6e0cadda
Karlis <curtislv@gmail.com>
2022-05-22 22:34:19 +0300

    22:33 Neural network visualized and linked to car's actions

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const networkCtx = networkCanvas.getContext('2d');
 console.log(networkCtx);
 
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, 'KEYS');
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, 'AI');
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2)];
 
 animate();
@@ -34,7 +34,7 @@ function animate(time) {
     car.draw(carCtx, 'blue');
     carCtx.restore();
 
-    networkCtx.lineDashOffset = -time / 50;
+    networkCtx.lineDashOffset = -time / 60;
     Visualizer.drawNetwork(networkCtx, car.brain);
     requestAnimationFrame(animate);
 }

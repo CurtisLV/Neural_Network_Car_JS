40098d240740ccd268d5dcb38e9d6f8187976aca
CurtisLV <curtislv@gmail.com>
2022-05-23 22:06:05 +0300

    22.05 Added several cars, save and discard to local storage, network mutations

--- sensor.js
+++ sensor.js
@@ -2,7 +2,7 @@ class Sensor {
     constructor(car) {
         this.car = car;
         this.rayCount = 5;
-        this.rayLength = 200;
+        this.rayLength = 150;
         this.raySpread = Math.PI / 2; // 45deg
 
         this.rays = [];
